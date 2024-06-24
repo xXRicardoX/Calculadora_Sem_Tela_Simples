bf7170e37ff6637be056f7c61f3ce072f41f5233
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2024-06-24 13:26:13 -0300

    chore: Add .idea and .gitignore files

---
+++ src/Calcular/Main.java
@@ -0,0 +1,11 @@
+package Calcular;
+
+import Calcular.operacoes.Calcular;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calcular.realizandoOperacoes();
+    }
+}
---
+++ src/Calcular/operacoes/Operadores.java
@@ -0,0 +1,27 @@
+package Calcular.operacoes;
+
+public class  Operadores {
+
+
+    public double somaOperadores(double valor_a, double valor_b) {
+        return (valor_a + valor_b);
+
+    }
+
+    public double subtracaoOperadores(double valor_a, double valor_b) {
+        return (valor_a - valor_b);
+
+    }
+
+    public double divisaoOperadores(double valor_a, double valor_b) {
+        return (valor_a / valor_b);
+
+    }
+
+    public double multiplicacaoOperadores(double valor_a, double valor_b) {
+        return (valor_a * valor_b);
+
+    }
+
+
+}
